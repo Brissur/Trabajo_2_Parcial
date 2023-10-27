1bbf59b31bc7e3ddb3fc8118e51b33d5451cd001
Bryan <bryanissur@gmail.com>
2023-10-27 08:26:56 -0600

    MK1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Validación del formulario de ingreso
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Aquí puedes agregar la lógica de validación del formulario de ingreso.
+    if (username === 'usuario' && password === 'contrasena') {
+        alert('Ingreso exitoso');
+    } else {
+        alert('Error: Usuario o contraseña incorrectos');
+    }
+});
+
+// Validación del formulario de registro
+document.getElementById('registrationForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const newUsername = document.getElementById('newUsername').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    // Aquí puedes agregar la lógica de validación del formulario de registro.
+    // Por ejemplo, puedes enviar los datos al servidor para el registro.
+
+    // Muestra un mensaje de registro exitoso (esto es solo un ejemplo).
+    alert('Registro exitoso para el usuario ' + newUsername);
+    // Cierra el modal de registro.
+    $('#registrationModal').modal('hide');
+});
